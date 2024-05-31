aff9bc713f6acd697d6be4f8ea6e55d60957a8c1
Vlad Malcev <vlad.xet@mail.ru>
2024-05-31 20:22:44 +0700

    4

--- Program.cs
+++ Program.cs
@@ -1,13 +1,9 @@
+using NServiceBus.Testing;
+
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
+app.UseWelcomePage();
 
-app.Run(async (context) =>
-{
-    var response = context.Response;
-    response.Headers.ContentLanguage = "ru-RU";
-    response.Headers.ContentType = "text/plain; charset=utf-8";
-    response.Headers.Append("secret-id", "256");    // добавление кастомного заголовка
-    await response.WriteAsync("ѕривет METANIT.COM");
-});
 
 app.Run();
+public delegate Task RequestDelegate(HttpContext context);
--- obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ea5b63f9fed9168e875133f513b48ff49249ac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
