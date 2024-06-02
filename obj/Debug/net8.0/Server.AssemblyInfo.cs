207a7fb8a4c5c9d22f8a8a940814dfd29b2df6b7
taysone11 <taysonereq@gmail.com>
2024-06-02 15:25:59 +0300

    sdfsdf

--- obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ea5b63f9fed9168e875133f513b48ff49249ac4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47d980aecd4602524bc09f82f85cef2de4115779")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
